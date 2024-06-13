c80e7d04630510350e4f61158d705dcd2a8093a4
Aline C. <alinecuba7@gmail.com>
2024-06-13 21:59:34 +0000

    Arrumando o erro na linha 7 - faltou o "as purchase_price".

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,9 +4,9 @@ SELECT
     orders_id,
     revenue, 
     quantity, 
-    CAST(purchase_price AS FLOAT64), 
-    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,s.revenue, 
-    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+    CAST(purchase_price AS FLOAT64) as purchase_price, 
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
 FROM {{ref("stg_raw__sales")}} s
 LEFT JOIN {{ref("stg_raw__product")}} p 
     USING (products_id)
